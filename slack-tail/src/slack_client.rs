b8d6181e9d7d0029c83b141c526f8fa022254ec5
Malcolm <malcolmbalch@gmail.com>
2020-10-03 15:05:08 -0400

    Set up tokio runtime and initialize generated client

--- slack-tail/src/slack_client.rs
+++ slack-tail/src/slack_client.rs
@@ -1,12 +1,22 @@
 use swagger::apis::*;
 use swagger::models::*;
 use swagger::apis::configuration::Configuration;
+use swagger::apis::client::APIClient;
+use hyper::Client;
+use tokio_core::reactor::Core;
 
 pub struct SlackClient {
 }
 
 impl SlackClient  {
     pub fn new() -> SlackClient {
+        let mut core = Core::new().unwrap();
+        let handle = core.handle();
+        let hyper_client = Client::new(&handle);
+        let configuration = Configuration::new(hyper_client);
+        let api_client = APIClient::new(configuration);
+        // TODO: Set up authentication, make a real request
+
         SlackClient {}
     }
 }
