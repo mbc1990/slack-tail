45b28d006cba9fd74ee3b96ebd9762798993dda5
Malcolm <malcolmbalch@gmail.com>
2020-10-04 16:02:10 -0400

    Remove token

--- slack-tail/src/lib.rs
+++ slack-tail/src/lib.rs
@@ -11,7 +11,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let oauth_access_token = "xoxb-472934603414-1418425791745-VmKqXWVQQLg1O0G7qdCwUuc1";
         let client = SlackClient::new(oauth_access_token);
         assert_eq!(2 + 2, 4);
     }
