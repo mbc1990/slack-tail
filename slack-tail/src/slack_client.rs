4dbcc46e246816e4b48a0f8a35eff76bb63cd416
Malcolm <malcolmbalch@gmail.com>
2020-10-04 17:02:03 -0400

    Cleanup, start writing tail client

--- slack-tail/src/slack_client.rs
+++ slack-tail/src/slack_client.rs
@@ -1,40 +1,23 @@
 use openapi::apis::configuration::Configuration;
 use openapi::apis::oauth_api::oauth_access;
 use openapi::apis::channels_api;
+use openapi::apis::conversations_api;
 use openapi::models::*;
 use tokio::runtime::Runtime;
 
 
-
 pub struct SlackClient {
-    configuration: Configuration
+    configuration: Configuration,
+    runtime: Runtime
 }
-
 impl SlackClient  {
     pub fn new(oauth_access_token: &str) -> SlackClient {
         let mut runtime = Runtime::new().expect("Failed to create Tokio runtime");
-
         let mut configuration = Configuration::new();
         configuration.oauth_access_token = Some(oauth_access_token.to_string());
-
-        let res_future = channels_api::channels_list(
-            &configuration,
-            Some(false),
-            None,
-            None,
-            None,
-           Some(true)
-        );
-
-        let res = runtime.block_on(res_future);
-        match res {
-            Ok(payload) => {
-                println!("good: {:?}", payload) ;
-            },
-            Err(err) => {
-                println!("Error: {:?}", err) ;
-            }
-        }
-        SlackClient {configuration}
+        SlackClient {configuration, runtime}
     }
+
+    // TODO: Tail #channel - returns a channel
+    // TODO: Send message, takes channel, string
 }
